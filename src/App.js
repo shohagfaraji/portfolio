98b0ba9ca01f3fe3cc2310b7ea289d933d5d825e
Shohag Faraji <shohagfaraji2@gmail.com>
2025-09-14 13:19:00 +0600

    Add Skills page

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Footer from "./components/Footer";
 // import Alert from "./components/Alert";
 import Home from "./components/Home";
 import About from "./components/About";
-// import Skills from "./components/Skills";
+import Skills from "./components/Skills";
 // import Projects from "./components/Projects";
 // import Achievements from "./components/Achievements";
 // import Contact from "./components/Contact";
@@ -28,7 +28,7 @@ function App() {
                     <Routes>
                         <Route exact path="/" element={<Home />} />
                         <Route exact path="/about" element={<About />} />
-                        {/* <Route exact path="/skills" element={<Skills />} /> */}
+                        <Route exact path="/skills" element={<Skills />} />
                         {/* <Route exact path="/projects" element={<Projects />} /> */}
                         {/* <Route
                             exact
